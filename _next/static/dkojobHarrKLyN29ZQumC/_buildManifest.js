682e45e6b9428fa413e96ce202a463bd259ea04b
Woo Dohyeong <wdh2100@gmail.com>
2022-06-26 02:20:46 +0900

    add resource prefix

---
+++ _next/static/dkojobHarrKLyN29ZQumC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/21be362571f00a9c.css","static/chunks/pages/index-57f2893786a4814a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
